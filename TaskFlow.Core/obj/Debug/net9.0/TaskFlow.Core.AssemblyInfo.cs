c5be302005e2fc30c6ef61d89d16b2980fbc9bee
BabaFakruddin <fareedemmubaigari@gmail.com>
2025-08-21 10:08:48 +0530

    final commit in setup

--- TaskFlow.Core/obj/Debug/net9.0/TaskFlow.Core.AssemblyInfo.cs
+++ TaskFlow.Core/obj/Debug/net9.0/TaskFlow.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb64f8fc7ae7129530577e4c1717e45e58ffb19a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
